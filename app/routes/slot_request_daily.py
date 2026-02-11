f24d5077f44fe748c19d1ef977ce5639a33d6f15
pparimi89 <pparimi89@gmail.com>
2026-02-11 13:25:22 +0530

    Seat Schedule changes

--- app/routes/slot_request_daily.py
+++ app/routes/slot_request_daily.py
@@ -1,12 +1,15 @@
-from typing import Optional
 from fastapi import APIRouter, Depends, Query
 from sqlalchemy.orm import Session
 
 from app.db.session import get_db
+from app.models.seat_schedule_request import SeatScheduleRequest
 from app.schemas.slot_requests_daily_response import SlotRequestsDailyResponse
+from app.services.seat_schedule_service import SeatScheduleService
 from app.services.slot_request_daily_service import create_slot_request_daily, get_by_airport
 from app.services.airport_service import get_airport_by_iata
 from app.schemas.response import ApiResponse
+from datetime import date, time
+from typing import List, Optional
 
 router = APIRouter()
 
@@ -57,4 +60,23 @@ def search_by_airport(
     return ApiResponse(
         message="Slot request daily search results",
         data={"airport_name": airport_name, "results": response_items}
+    )
+
+
+@router.post("/seat-schedules/view")
+def seat_schedule_view(
+    request: SeatScheduleRequest,
+    db: Session = Depends(get_db)
+):
+    return SeatScheduleService.get_seat_schedule_view(
+        db=db,
+        schedule=request.schedule,
+        from_date=request.from_date,
+        to_date=request.to_date,
+        excluding_airline=request.excluding_airline,
+        terminal=request.terminal,
+        days=request.days,
+        slot_start=request.slot_start,
+        slot_end=request.slot_end,
+        hours = request.hours
     )
